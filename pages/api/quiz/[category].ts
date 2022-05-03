61a422a21f727c9cb27bf20743a0d1941647fd8d
Dom <dom.m27@outlook.com>
2022-05-03 16:09:23 +0100

    Fixed a TS error.

--- pages/api/quiz/[category].ts
+++ pages/api/quiz/[category].ts
@@ -1,17 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import {IQuiz} from "../../../utils/structs/Quiz";
 import APIError from '../../../utils/error/APIError';
 import ErrorType from '../../../utils/error/ErrorType';
 import MQuiz from '../../../utils/models/MQuiz';
 import { connect } from '../../../utils/connection';
 import { methodGuard } from '../../../utils/general';
+import { IQuizPartial } from '../../../utils/structs/Quiz';
 
 
 
 
 const mQuiz = new MQuiz();
-export default async function handler(req: NextApiRequest, res: NextApiResponse<IQuiz | APIError>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<IQuizPartial | APIError>) {
     await connect(); //Wait for database conn.
 
     if (!methodGuard(["GET"], req, res)){
