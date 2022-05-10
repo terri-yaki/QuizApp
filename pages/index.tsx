25e5451e13a124aa1f8ef2b08f225bdeb0b4fe19
Liam Cross <lc01383@surrey.ac.uk>
2022-05-10 12:51:14 +0100

    removing scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,6 @@ import ExampleComponent from '../components/ExampleComponent/ExampleComponent';
 import TwitterStream from '../components/TwitterStream/TwitterStream'
 
 const Home: NextPage = () => {
-    // Twitter stream scroll functionality
-    if (typeof window !== 'undefined') {
-        let stream = document.getElementById('twitter-stream');
-        setInterval(() => {
-            if (stream != null) {
-                stream.scroll({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-        }, 13000)
-    }
-
     return (
         <div id={'root'}>
             <ExampleComponent/>
